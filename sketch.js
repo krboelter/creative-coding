c1f4ce822a2c373eef28f468831228daa81cce9d
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-03-25 21:27:38 -0600

    changed some settings

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const random = require("canvas-sketch-util/random");
 const palettes = require("nice-color-palettes");
 
 const settings = {
-  dimensions: [2048, 2048]
+  dimensions: [2048, 2048],
 };
 
 const sketch = () => {
@@ -12,22 +12,24 @@ const sketch = () => {
 
   const createGrid = () => {
     const points = [];
-    const count = 30;
+    const count = 40;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v) * 0.2);
         points.push({
           color: random.pick(palette),
-          radius: Math.abs(0.01 + random.gaussian() * 0.01),
-          position: [u, v]
+          radius,
+          rotation: random.noise2D(u, v),
+          position: [u, v],
         });
       }
     }
     return points;
   };
 
-  random.setSeed(512); // makes random (from canvas-sketch-util) be the same random every time
+  // random.setSeed(512); // makes random (from canvas-sketch-util) be the same random every time
   const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 400;
 
@@ -35,18 +37,26 @@ const sketch = () => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    points.forEach(data => {
-      const { position, radius, color } = data;
+    points.forEach((data) => {
+      const { position, rotation, radius, color } = data;
       const [u, v] = position;
 
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.fillStyle = color;
+      // context.lineWidth = 10;
+      // context.fill();
+
+      context.save();
       context.fillStyle = color;
-      context.lineWidth = 10;
-      context.fill();
+      context.font = `${radius * width}px 'Helvetica'`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText("KEN", 0, 0);
+      context.restore();
     });
   };
 };
